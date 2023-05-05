5c1b2ecd6c6df951b5ee0fd5036adfded66313cd
MPythonS <starkus.mantas@gmail.com>
2023-05-05 19:21:26 +0300

    Asilas.py pridėtas metodas dainuoti:     def dainuoti(self):         print("Unt Sartų unt Sartų lakia roges ratu")

--- Asilas.py
+++ Asilas.py
@@ -14,6 +14,9 @@ class Asilas(MitineButybe):
     def valgyti(self):
         self.jegos += 20
 
+    def dainuoti(self):
+        print("Unt Sartų unt Sartų lakia roges ratu")
+
 
 asilas1 = Asilas("Uni")
 asilas2 = Asilas("Shiny")
