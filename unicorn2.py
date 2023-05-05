ff6ff61d9fc30cd37c2865705d59892b18c27964
aivaraskriksciunas <aiviskri@gmail.com>
2023-05-05 18:34:47 +0300

    Pridejau gyvuna nauja

---
+++ unicorn2.py
@@ -0,0 +1,26 @@
+from mythical import MitineButybe
+
+
+class Vienaragis(MitineButybe):
+
+    def __init__(self, vardas):
+        super().__init__(vardas)
+        self.jegos = 1000
+
+    def keliauti(self, vietove):
+        self.vietove = vietove
+        self.jegos -= 20
+
+    def valgyti(self):
+        self.jegos += 20
+
+
+vienaragis1 = Vienaragis("Uni")
+vienaragis2 = Vienaragis("Shiny")
+
+vienaragis1.dainuoti()
+vienaragis2.keliauti("Kaunas")
+vienaragis2.valgyti()
+vienaragis2.valgyti()
+print(vienaragis2.jegos)
+print(vienaragis2.vietove)
